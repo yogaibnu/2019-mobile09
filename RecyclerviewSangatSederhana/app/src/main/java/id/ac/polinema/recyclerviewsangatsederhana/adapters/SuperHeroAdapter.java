c1520cd0e728a8107af8ad04f7f31ba307dad4fd
yogaibnu <yogaibnudinataa@gmail.com>
2019-10-22 21:16:33 +0700

    Recyclerview Sederhana

---
+++ RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/adapters/SuperHeroAdapter.java
@@ -0,0 +1,65 @@
+package id.ac.polinema.recyclerviewsangatsederhana.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import id.ac.polinema.recyclerviewsangatsederhana.R;
+import id.ac.polinema.recyclerviewsangatsederhana.models.SuperHero;
+
+public class SuperHeroAdapter extends RecyclerView.Adapter<SuperHeroAdapter.MyViewHolder> {
+    List<SuperHero> heroList;
+
+    public SuperHeroAdapter(List<SuperHero> heroList) {
+        this.heroList = heroList;
+    }
+
+    @NonNull
+    @Override
+    public SuperHeroAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //object context diambil dari parent
+        Context context = parent.getContext();
+        //object context digunakan untuk membuat object layoutInflater
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        //object layoutInflater digunakan untuk membuat object view yang merupakan hasil inflate  layout ( mengubungkan adapter degnan layout)
+        View superHeroView = layoutInflater.inflate(R.layout.item_super_hero,parent,false);
+        //object superHeroView digunakan untuk membuat object viewHolder
+        MyViewHolder viewHolder = new MyViewHolder(superHeroView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SuperHeroAdapter.MyViewHolder holder, int position) {
+        //ambil satu item hero
+        SuperHero hero = heroList.get(position);
+        //set text heroName berdasarkan data dari model hero
+        holder.heroName.setText(hero.getHeroName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (heroList != null) ? heroList.size() : 0;
+        /*for non shorthand people*/
+        /*if(heroList!=null){
+          return heroList.size();
+        }else{
+          return 0;
+        }*/
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView heroName;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+//            TextView heroName = itemView.findViewById(R.id.heroName);
+            heroName = itemView.findViewById(R.id.heroName);
+        }
+    }
+}
---
+++ RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/models/SuperHero.java
@@ -0,0 +1,17 @@
+package id.ac.polinema.recyclerviewsangatsederhana.models;
+
+public class SuperHero {
+    public String heroName;
+
+    public SuperHero(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+}
